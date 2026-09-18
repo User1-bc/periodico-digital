43ac9dc167e69e3f3865707c4b29fe9fd143e1cd
User1-bc <bcarvajal1129@gmail.com>
2026-09-17 23:54:15 -0400

    Usar Internal Database URL de Render

--- conexion.php
+++ conexion.php
@@ -1,11 +1,14 @@
 <?php
-$host = 'dpg-dama3o6legvs738n12g0-a.virginia-postgres.render.com';
-$db   = 'periodico_7d5x';
-$user = 'periodico_7d5x_user';
-$pass = 'W4IQqdXFVaFOC12NqwtWH61JM1h7PTzn';
+$url = parse_url("postgresql://periodico_7d5x_user:W4IQqdXFVaFOC12NqwtWH61JM1h7PTzn@dpg-dama3o6legvs738n12g0-a.virginia-postgres.render.com:5432/periodico_7d5x");
+
+$host = $url["host"];
+$port = $url["port"] ?? "5432";
+$db   = ltrim($url["path"], "/");
+$user = $url["user"];
+$pass = $url["pass"];
 
 try {
-    $dsn = "pgsql:host=$host;port=5432;dbname=$db;sslmode=require";
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
