2264cdcafd29871bbafd6e6d8772ef67b228adb0
User1-bc <bcarvajal1129@gmail.com>
2026-09-22 22:07:44 -0400

    Add cache buster v3

--- index.php
+++ index.php
@@ -1408,7 +1408,8 @@ function updateSuscripcionButtonState(active) {
 
 </body>
 </html>
-<?php /* cache-buster: 2024-09-23-01 */ ?>
+<?php /* cache-buster: 2024-09-23-03 */ ?>
+<?php // End of file ?>
 
 
 
